8afb6fea96256b17518d139003ec51ad5a082c55
malak-zangar <maleeqzangar@gmail.com>
2024-08-12 14:40:06 +0100

    profile done

--- auth/model.py
+++ auth/model.py
@@ -1,17 +1,37 @@
+import time
+import os
+from flask import current_app
 from flask_login import UserMixin
+import jwt
 from db import db
-#from app import login_manager
 
 class Auth(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+    email= db.Column(db.String(150),unique=True,  nullable=False)
 
+    def get_reset_token(self, expires_sec=1800): 
+  
+        return jwt.encode({'reset_password': self.username,
+                           'exp': time.time() + expires_sec},
+                           key=os.getenv('SECRET_KEY'))
+
+    def verify_reset_token(token):
+        try:
+            username = jwt.decode(token,
+              key=os.getenv('SECRET_KEY'))['reset_password']
+        except Exception as e:
+            print(e)
+            return
+        return Auth.query.filter_by(username=username).first()
+    
     def serialize(self):
         return {
                 'id': self.id,
                 'username': self.username,
                 'password': self.password,
+                'email': self.email,
         }
 
     def is_active(self):
--- auth/view.py
+++ auth/view.py
@@ -1,5 +1,9 @@
 from datetime import timedelta
-from flask import Blueprint, request, jsonify, make_response
+import os
+from flask import Blueprint, current_app, request, jsonify, make_response
+from flask_mail import Message
+import jwt
+import validators
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from auth.model import Auth
@@ -23,10 +27,17 @@ def login():
     user = Auth.query.filter_by(username=username).first()
     
     if user and check_password_hash(user.password, password):
-        access_token = create_access_token(identity=user.username, expires_delta=timedelta(hours=2))
-        return jsonify(access_token=access_token), 200
+        access_token = create_access_token(identity={'id': user.id, 'username': user.username}, expires_delta=timedelta(hours=2))
+        return jsonify({
+            'access_token': access_token,
+            'user': {
+                'id': user.id,
+                'username': user.username,
+                'email' : user.email
+            }
+        }), 200
     else:
-        return jsonify({"erreur": "Invalid username or password"}), 401
+        return jsonify({"erreur": "Invalid username or password"}), 402
 
 # Add new admin
 @auth.route('/create', methods=['POST'])
@@ -34,19 +45,31 @@ def create_admin():
     data = request.get_json()
     username = data.get("username")
     password = data.get("password")
+    email = data.get("email")
 
-    if not (username and password):
+    if not (username and password and email):
         return jsonify({
-            "erreur": "svp entrer un nom d'utilisateur et mot de passe"
+            "erreur": "svp entrer un nom d'utilisateur , un email et un mot de passe"
         }), 400
     
     if len(username) < 3:
         return jsonify({'erreur': "Nom d'utilisateur trop court"}), 400
 
-    if not username.isalnum() or " " in username:
-        return jsonify({'erreur': "nom d'utilisateur ne doit contenir que des caractères et pas d'espace "}), 400
+    if not username.isalnum() :
+        return jsonify({'erreur': "nom d'utilisateur ne doit contenir que des caractères"}), 400
+    
+    if Auth.query.filter_by(username=username).first() is not None:
+        return jsonify({'erreur': "Nom d'utilisateur existe déja"}), 409
+    
+    if not validators.email(email) :
+        return jsonify({'error': "Email is not valid"}), 400
+    
+    if Auth.query.filter_by(email=email).first() is not None:
+        return jsonify({'erreur': "Email existe déja"}), 409
 
-    new_admin = Auth(username=username, password=generate_password_hash(password, method='sha256'))
+
+
+    new_admin = Auth(username=username,email=email, password=generate_password_hash(password, method='sha256'))
     db.session.add(new_admin)
     db.session.commit()
     return make_response(jsonify({"message": "admin created successfully", "admin": new_admin.serialize()}), 201)
@@ -56,8 +79,116 @@ def create_admin():
 def logout():
     return 'user logged out successfully'
 
-@auth.route("/protected", methods=['GET'])
+# @auth.route("/protected", methods=['GET'])
+# @jwt_required()
+# def protected():
+#     current_user_id = get_jwt_identity()
+#     return jsonify(logged_in_as=current_user_id), 200
+
+
+#ModifierProfil
+@auth.route('/profile/<int:id>',methods=['PUT'])
 @jwt_required()
-def protected():
-    current_user_id = get_jwt_identity()
-    return jsonify(logged_in_as=current_user_id), 200
+def updateProfil(id):
+    user = Auth.query.get(id)
+
+    if not user:
+        return jsonify({"message": "user n'existe pas"}), 404
+    data = request.get_json()
+    print(data)
+    password = data.get("password")
+    user.email = data.get("email",user.email)
+    user.username = data.get("username",user.username)
+
+    if password : 
+        user.password = generate_password_hash(password, method='sha256')
+
+    try:
+        db.session.commit()
+        return jsonify({"message": "Utilisateur modifié avec succés"}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"message": "Echec dans la modification de l'utilisateur "}), 500
+
+#updatePassword
+@auth.route('/resetpassword',methods=['PUT'])
+def resetPassword():
+    data = request.get_json()
+    print(data)
+    token = data.get('token')
+    print(token)
+    password = data.get('password')
+    print(password)
+   
+
+    if not token:
+        return jsonify({"message": "Token invalide ou expiré"}), 402
+    if not password:
+        return jsonify({"message": "Mot de passe requis"}), 400
+
+
+    try:
+        decoded_data = jwt.decode(token, key=os.getenv('SECRET_KEY'), algorithms=["HS256"])
+        username = decoded_data['reset_password']
+
+        # Trouvez l'utilisateur par son nom d'utilisateur
+        user = Auth.query.filter_by(username=username).first()
+        if not user:
+            return jsonify({"message": "Utilisateur non trouvé"}), 404
+        user.password = generate_password_hash(password, method='sha256')
+
+
+
+        db.session.commit()
+        return jsonify({"message": "Mot de passe réinitialisé avec succés"}), 200
+    except jwt.ExpiredSignatureError:
+        return jsonify({"message": "Le token a expiré"}), 400
+    except jwt.InvalidTokenError:
+        return jsonify({"message": "Token invalide"}), 400
+    except Exception as e:
+        print(f"Erreur: {e}")
+        db.session.rollback()
+        return jsonify({"message": "Echec dans la réinitalisation du mot de passe "}), 500
+
+#SendRecuperationMail
+@auth.route('/sendmail',methods=['POST'])
+def send_recuperation_email():
+    from app import mail
+    data = request.get_json()
+    print(data)
+
+    email=data.get("email")
+    if not email:
+        return jsonify({"message": "Adresse email manquante"}), 400
+    print(email)
+
+    auth=Auth.query.filter_by(email=email).first()
+    if auth :
+        token = auth.get_reset_token()
+        try : 
+            msg = Message(
+                "Récupération du mot de passe",
+                sender=current_app.config['MAIL_USERNAME'],
+                recipients=[email]
+            )
+            reset_url = f"http://localhost:5173/resetPassword?token={token}"
+            msg.body = f"Bonjour Monsieur/Madame {auth.username}, veuillez cliquer sur ce lien pour réinitialiser votre mot de passe : {reset_url}"               
+            mail.send(msg)
+            return jsonify({"message": "Email envoyé avec succès"}), 200
+        except Exception as e:
+            print(f"Erreur lors de l'envoi de l'email: {e}")
+            return jsonify({"erreur": "Échec de l'envoi de l'email"}), 500
+    else :
+        return jsonify({"erreur": "Adresse email n'existe pas"}), 402
+
+@auth.route('/verify-password/<int:id>', methods=['POST'])
+@jwt_required()
+def verify_password(id):
+    data = request.get_json()
+    current_password = data.get('password')
+
+    user = Auth.query.get(id)
+    if not user or not check_password_hash(user.password, current_password):
+        return jsonify({"message": "Mot de passe incorrect"}), 402
+
+    return jsonify({"message": "Mot de passe correct"}), 200
